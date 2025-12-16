396255e0551cbf812d938547218cc71b4146a3a4
Yalog <nguyenyalog@gmail.com>
2025-12-16 22:50:56 +0700

    Build board

---
+++ pacman.py
@@ -0,0 +1,77 @@
+from board import boards
+import pygame
+import math
+
+pygame.init()
+
+WIDTH, HEIGHT = 900, 950
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+level = boards
+color = 'pink'
+PI = math.pi
+player_images = []
+for i in range(1, 5):
+    player_images.append(pygame.transform.scale(pygame.image.load(f'assets/player_images/{i}.png'), (45, 45)))
+blinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/red.png'), (45, 45))
+pinky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/pink.png'), (45, 45))
+inky_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/blue.png'), (45, 45))
+clyde_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/orange.png'), (45, 45))
+spooked_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/powerup.png'), (45, 45))
+dead_img = pygame.transform.scale(pygame.image.load(f'assets/ghost_images/dead.png'), (45, 45))
+
+def draw_board():
+    num1 = (HEIGHT - 50) // 32
+    num2 = WIDTH // 30
+
+    for i in range(len(level)):
+        for j in range(len(level[i])):
+            if level[i][j] == 1:
+                pygame.draw.circle(screen, 'white', ((j*num2 + (0.5*num2)), (i*num1 + (0.5*num1))), 4)
+
+            if level[i][j] == 2:
+                pygame.draw.circle(screen, 'white', ((j*num2 + (0.5*num2)), (i*num1 + (0.5*num1))), 10)
+
+            if level[i][j] == 3:
+                pygame.draw.line(screen, color, (j*num2 + (0.5*num2), i * num1),
+                                                            (j*num2 + (0.5*num2), i * num1 + num1), 3)
+            if level[i][j] == 4:
+                pygame.draw.line(screen, color, (j*num2, i*num1 + (0.5*num1)),
+                                                            (j*num2 + num2, (i*num1 + (0.5*num1))), 3)
+            if level[i][j] == 5:
+                pygame.draw.arc(screen, color, [(j*num2 - (num2*0.4) - 2), (i*num1 + (0.5*num1)), num2, num1], 0, PI/2, 3)
+
+            if level[i][j] == 6:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 + (num2 * 0.5)), (i * num1 + (0.5 * num1)), num2, num1], PI / 2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(screen, color, [(j * num2 + (num2 * 0.5)), (i * num1 - (0.4 * num1)), num2, num1], PI,
+                                3 * PI / 2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(screen, color,
+                                [(j * num2 - (num2 * 0.4)) - 2, (i * num1 - (0.4 * num1)), num2, num1], 3 * PI / 2,
+                                2 * PI, 3)
+            if level[i][j] == 9:
+                pygame.draw.line(screen, 'white', (j * num2, i * num1 + (0.5 * num1)),
+                                                            (j * num2 + num2, (i * num1 + (0.5 * num1))), 3)
+
+def draw_player():
+    pass
+
+run = True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+    draw_board()
+    draw_player()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit()
+
+
